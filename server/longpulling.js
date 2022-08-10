bf659448dd1ca5fb0145a35c8f7668e673a86a7f
Andriy Mriy <andriymriy@gmail.com>
2022-08-10 23:08:17 +0300

    init

---
+++ server/longpulling.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cors = require('cors');
+const events = require('events');
+const PORT = 3010;
+
+const emitter = new events.EventEmitter();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/get-messages', (req, res) => {
+  emitter.once('newMessage', (message) => {
+    res.json(message);
+  });
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.post('/new-messages', (req, res) => {
+  const message = req.body;
+  emitter.emit('newMessage', message);
+  res.status(200);
+});
+
+app.listen(PORT, () => console.log(`server started on port ${PORT}`));
